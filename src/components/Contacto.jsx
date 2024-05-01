3a7605fabfe3cf9940d2fec4005c4b7511177a07
Joel Herrera Leiva <71947618+Joelherr09@users.noreply.github.com>
2024-05-01 03:15:54 -0400

    asdasd

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -3,6 +3,9 @@ import emailjs from '@emailjs/browser';
 import { FaGithubSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Contacto = () => {
 
     const form = useRef();
@@ -24,9 +27,17 @@ const Contacto = () => {
         );
     };    
 
+    const notify = () => {
+       //toast("Correo enviado!");
+        toast.success("Correo enviado!", {
+            position: "top-center"
+          });
+    }
+
   return (
     
-    <div className='w-[90%] mx-auto text-white bg-slate-700/80 drop-shadow-lg shadow-lg shadow-blue-500/50 p-3 my-6'>
+    <div className='w-[100%] md:w-[70%] mx-auto text-white bg-slate-700/80 drop-shadow-lg shadow-lg shadow-blue-500/50 p-3 my-6'>
+        
         <h1 className='text-center text-2xl font-semibold md:text-3xl md:font-black'>Contáctame</h1>
         <div className=' w-full mx-auto justify-center flex flex-col items-center'>
             <h1 className='text-center text-2xl font-bold py-4 text-red-600'>joelherreraleiva@gmail.com</h1>
@@ -34,8 +45,9 @@ const Contacto = () => {
 
             <div className='w-[90%]'>
                 <form ref={form} onSubmit={sendEmail} id="contact-form" className='flex flex-col w-full'>
+                
                     <input type="hidden" name="contact_number" value="697483" />
-                    <div className='flex w-full gap-2'>
+                    <div className='flex flex-col md:flex-row w-full gap-2'>
                         <div className='flex flex-col w-full'>
                             <label>Nombre</label>
                             <input type="text" name="user_name" className='text-black p-1' />
@@ -48,7 +60,8 @@ const Contacto = () => {
 
                     <label>Mensaje</label>
                     <textarea name="message" rows="5" className='text-black p-1'></textarea>
-                    <input type="submit" value="Enviar" className='bg-[#130f40] py-1 my-2' />
+                    <input onClick={notify} type="submit" value="Enviar" className='bg-[#130f40] py-1 my-2' /><ToastContainer />
+                    
                 </form>
             </div>
 
@@ -56,6 +69,7 @@ const Contacto = () => {
                 <a href="https://twitter.com/joelherr09" target="_blank" rel="noreferrer"><FaSquareXTwitter size={36} className='hover:scale-105 duration-300 hover:shadow-lg hover:shadow-blue-500/50 rounded-lg'/></a>
                 <a href="https://github.com/Joelherr09" target="_blank" rel="noreferrer"><FaGithubSquare size={36} className='hover:scale-105 duration-300 hover:shadow-lg hover:shadow-blue-500/50 rounded-lg'/></a>
             </div>
+            <ToastContainer />
         </div>
     </div>
   );
