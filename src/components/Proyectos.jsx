532d1de60cffe03be1a9633af19b104ceab715a8
Joel Herrera Leiva <71947618+Joelherr09@users.noreply.github.com>
2024-05-02 02:02:55 -0400

    act

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -9,9 +9,9 @@ const Proyectos = () => {
   return (
     <div id="portafolio" className="w-full md:py-4 py-6">
         <h1 className="text-center text-2xl font-bold text-white md:text-3xl md:font-black">Proyectos Realizados</h1>
-      <div className="w-[90%] mx-auto text-white  p-2 flex flex-col gap-4 md:gap-3 md:grid md:grid-cols-2">
+      <div className="w-[90%] mx-auto text-white  p-2 flex flex-col gap-4 md:gap-4 md:grid md:grid-cols-2">
         
-        <div className="bg-[#0c2c27] drop-shadow-lg shadow-lg shadow-black p-3 hover:scale-105 duration-300">
+        <div className="bg-[#0c2c27] drop-shadow-lg shadow-lg shadow-black p-3 hover:scale-105 duration-300 hover:bg-[#27524b]">
           <a href="https://www.vendepo.cl/" target="_blank" rel="noreferrer">
             <img src={Vendepo} alt="" />
             <div className="py-1">
@@ -24,7 +24,7 @@ const Proyectos = () => {
             </div>
           </a>
         </div>
-        <div className="bg-[#0c2c27] drop-shadow-lg shadow-lg shadow-black p-3 hover:scale-105 duration-300">
+        <div className="bg-[#0c2c27] drop-shadow-lg shadow-lg shadow-black p-3 hover:scale-105 duration-300 hover:bg-[#27524b]">
           <a href="https://www.cuartavoleibol.online/" target="_blank" rel="noreferrer">
             <img src={CVSS} alt="" />
             <div className="py-1">
@@ -37,7 +37,7 @@ const Proyectos = () => {
             </div>
           </a>
         </div>
-        <div className="bg-[#0c2c27] drop-shadow-lg shadow-lg shadow-black p-3 hover:scale-105 duration-300">
+        <div className="bg-[#0c2c27] drop-shadow-lg shadow-lg shadow-black p-3 hover:scale-105 duration-300 hover:bg-[#27524b]">
           <a href="https://programaconerick.site/" target="_blank" rel="noreferrer">
             <img src={Cabana} alt="" />
             <div className="py-1">
@@ -50,7 +50,7 @@ const Proyectos = () => {
             </div>
           </a>
         </div>
-        <div className="bg-[#0c2c27] drop-shadow-lg shadow-lg shadow-black p-3 hover:scale-105 duration-300">
+        <div className="bg-[#0c2c27] drop-shadow-lg shadow-lg shadow-black p-3 hover:scale-105 duration-300 hover:bg-[#27524b]">
           <a href="https://ecommercechile.netlify.app/" target="_blank" rel="noreferrer">
             <img src={Ecommerce} alt="" />
             <div className="py-1">
